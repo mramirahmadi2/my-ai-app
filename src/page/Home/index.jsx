5b3ca4ca521a5c510a4fa48fba0a588b2d7fda10
mramirahmadi2 <amirahmadim24@gmail.com>
2025-04-30 17:01:53 +0330

    add new image

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
 
-const index = () => {
+import Style from './Style.module.css'
+const Home = () => {
   return (
-    <div>
-      hello
+    <div className="h-screen">
+      <div className={`${Style.firstImage} h-1/3 w-full`}>
+         <span className="text-white text-2xl font-bold">I&apos;m your private doctor</span>
+      </div>
+      <div className={`${Style.secondImage} h-1/3 w-full`}>
+        <span className="text-white text-2xl font-bold">form</span> 
+        <button>چت با ai</button>
+        <button>فرم پزشکی</button>
+      </div>
     </div>
   )
 }
 
-export default index
+export default Home
